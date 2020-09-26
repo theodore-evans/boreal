b0df86de3cb2e6139a4840ae1a982bb8a6f2ab82
Theodore Evans <theodore.evans@dai-labor.de>
2020-09-26 12:40:00 +0200

    Unit-based pathfinding is working

---
+++ Assets/Scripts/Pathfinding/IAStar.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public interface IPathfinding
+{
+    void StartFindPath(Vector3 startPos, Vector3 targetPos);
+}
---
+++ Assets/Scripts/World/INormalCalculator.cs
@@ -0,0 +1,4 @@
+﻿public interface INormalCalculator
+{
+    void Initialise(WorldController wc);
+}
